1f82be4488a1d9590dadef9a4b50494e98092d33
William Mendonça <william@tecmasternet.com.br>
2020-11-19 12:56:16 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+require('react-native').unstable_enableLogBox();
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
